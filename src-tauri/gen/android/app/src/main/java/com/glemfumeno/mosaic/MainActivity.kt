031c3a4c454c8957b2c42365f646ef97b47a5cba
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2026-01-17 17:21:19 +0100

    initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/glemfumeno/mosaic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.glemfumeno.mosaic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
